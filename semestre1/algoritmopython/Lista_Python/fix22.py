ea15502e6a2ff5f5d1713f9681019bead9065c22
ElisaaMartins <liaparecidamartinsoliveira@gmail.com>
2024-03-31 22:21:19 -0300

    lista de prática - Python

---
+++ semestre1/algoritmopython/Lista_Python/fix22.py
@@ -0,0 +1,22 @@
+compra1 = float(input("Digite o valor da primeira compra: "))
+compra2 = float(input("Digite o valor da segunda compra: "))
+compra3 = float(input("Digite o valor da terceira compra: "))
+
+total = compra1 + compra2 + compra3
+
+desc1 = total - (total * 0.20) 
+desc2 = total - (total * 0.15) 
+desc3 = total - (total * 0.10) 
+
+if (total >= 300):
+    print("Sua compra saiu de", total, "por:", desc1)
+elif ((total >= 200) & (total <=300)):
+    print("Sua compra saiu de", total, "por:", desc2)
+elif ((total >= 100) & (total <=200)):
+    print("Sua compra saiu de", total, "por:", desc3)
+else:
+    print("Sua compra não possui desconto")
+
+print ("Elisa Aparecida Martins de Oliveira - RA: 105392411023")
+
+    
---
+++ semestre1/algoritmopython/Lista_Python/fix25.py
@@ -0,0 +1,33 @@
+# usando math.ceil(), uma biblioteca do Python para fazer o arredondamento(para cima) dos resutados, pq a divisão por parte inteira (//) não vai
+import math 
+
+area = float(input("Digite a área que será pintada: "))
+
+# é dividido por 6 pq 1 lata cobre 6m^2
+quantDeTinta = (area / 6) + (10 * 0.10)
+
+# situação a
+quantLata = math.ceil(quantDeTinta / 18)
+precoLata = quantLata * 80
+
+# situação b
+quantGalao = math.ceil(quantDeTinta / 3.6)
+precoGalao = quantGalao * 35
+
+# situação c
+misturaLata = math.ceil(quantDeTinta / 18)
+misturaGalao = math.ceil((quantDeTinta % (misturaLata * 18)) / 3.6)
+precoGalaoLata = (misturaGalao * precoGalao) + (misturaLata * precoLata)
+
+print("Quantidade de tinta a ser comprada:", quantDeTinta, "litros")
+print("Situação a) Comprar apenas latas de 18 litros: ", quantLata, 
+      "latas - Preço total: R$", precoLata)
+print("Situação b) Comprar apenas galões de 3,6 litros: ", quantGalao, 
+      "galões - Preço total: R$", precoGalao)
+print("Situação c) Misturar latas e galões para o menor preço: ", misturaLata, 
+      "latas e ", misturaGalao, "galões - Preço total: R$ ", precoGalaoLata)
+
+print ("Elisa Aparecida Martins de Oliveira - RA: 105392411023")
+
+
+
