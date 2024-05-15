cf274d2ca829f9b94e7a52424d6eae182c2f158b
ElisaaMartins <liaparecidamartinsoliveira@gmail.com>
2024-05-14 23:22:00 -0300

    Lista prática 6 - Python

--- semestre1/algoritmopython/Lista_Python_6/fix62/fix62.py
+++ semestre1/algoritmopython/Lista_Python_6/fix62/fix62.py
@@ -2,13 +2,12 @@ arq = open('./Lista_Python_6/fix62/emails.txt', 'r', encoding="utf-8")
 email = arq.read()
 
 lista_email = email.split('\n')
+print(arq.read())
 
 for _ in range(3):
     novo_email = str(input("Digite um email aqui: "))
     lista_email.append(novo_email)
     print("Emails adicionados com sucesso!")
-
-print(arq.read())
 
 print (lista_email)
 print("Elisa Aparecida Martins de Oliveira - RA: 1051392411023 - Turma DSM 1°Semestre")
